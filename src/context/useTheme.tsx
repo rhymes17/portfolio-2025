764339f6811c57bb6e96a3a6c1ecf0a7127fa61c
rhymes17 <devrahul1709@gmail.com>
2024-12-22 01:45:59 +0530

    chore: theme context api

---
+++ src/context/useTheme.tsx
@@ -0,0 +1,47 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+type ThemeContextType = {
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
+};
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }, [isDarkMode]);
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        isDarkMode,
+        toggleDarkMode,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = (): ThemeContextType => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeContext');
+  }
+  return context;
+};
