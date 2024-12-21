da93a75f19ad19c7f8816855bd57430b692d84da
rhymes17 <devrahul1709@gmail.com>
2024-12-21 20:54:35 +0530

    chore: font-family setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1 className="text-2xl text-red-400">Rahul Dev Choudhari</h1>
+      <h1 className="text-2xl text-red-400 font-montserrat">Rahul Dev Choudhari</h1>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      montserrat: ['Montserrat', "sans-serif"],
+      outfit: ['Outfit', "sans-serif"],
+      'name-text': ['Mukta', "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
