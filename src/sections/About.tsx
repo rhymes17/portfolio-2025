83723a8c0a90734348f10f09b0b389dc3b787b88
rhymes17 <devrahul1709@gmail.com>
2024-12-22 15:44:36 +0530

    chore: intro section

---
+++ src/sections/About.tsx
@@ -0,0 +1,10 @@
+import { motion } from 'motion/react';
+const About = () => {
+  return (
+    <motion.div className="h-[100vh]">
+      <h1 className="text-section-title">ABOUT</h1>
+    </motion.div>
+  );
+};
+
+export default About;
