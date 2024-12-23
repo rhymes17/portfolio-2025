7d9aa5ea3cb106cdfddba74c8dc60848eb068214
rhymes17 <devrahul1709@gmail.com>
2024-12-23 15:09:51 +0530

    chore: spacing updates in experience section

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -19,16 +19,19 @@ const ExperienceCard = ({ data, hasBorderBottom = true }: Props) => {
           ? 'none'
           : '1px solid  rgb(138 138 111 / 0.5)',
       }}
-      className="flex h-[30vh] flex-col justify-center gap-[0.7rem] border-b-section-title/50 font-outfit md:gap-3 lg:flex-row lg:justify-evenly"
+      className="flex h-[30vh] flex-col justify-center gap-[0.8rem] border-b-section-title/50 font-outfit transition-all duration-700 md:gap-3 lg:flex-row lg:justify-evenly"
     >
       <h1 className="text-sm md:text-xl lg:flex-1 lg:text-3xl">{duration}</h1>
 
-      <div className="flex flex-col gap-[0.2rem] leading-[1rem] md:gap-2 md:leading-none lg:flex-1 lg:gap-3">
-        <h1 className="text-base md:text-3xl lg:text-4xl">{companyName}</h1>
-        <h4 className="text-xs font-light text-white/60 md:text-base">
-          {location}
-        </h4>
-        <h4 className="text-xs text-white/60 md:text-lg lg:text-base">
+      <div className="flex flex-col gap-[0.35rem] leading-none md:gap-2 md:leading-none lg:flex-1 lg:gap-3">
+        <div className="flex items-center gap-3 lg:flex-col lg:items-start lg:gap-3">
+          <h1 className="text-base md:text-3xl lg:text-4xl">{companyName}</h1>
+          <div className="h-[6px] w-[6px] rounded-full bg-section-title/70 lg:hidden"></div>
+          <h4 className="text-xs font-light text-white/60 md:text-base">
+            {location}
+          </h4>
+        </div>
+        <h4 className="text-sm font-semibold text-white/70 md:text-lg lg:text-lg lg:font-normal">
           {position}
         </h4>
       </div>
